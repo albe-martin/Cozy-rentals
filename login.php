34b860ed5fe90af778b6caf4630846923dc9bcbc
PGN9 <pigeonQ0627@gmail.com>
2024-03-31 22:44:08 -0600

    login base code

--- login.php
+++ login.php
@@ -1,5 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: tan;
+  color: white;
+}
+
+</style>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,5 +34,12 @@
     <title>Log In</title>
 </head>
 <body>
+    <div class="topnav">
+        <a href="index.php">Index</a>
+        <a href="propertylist.php">Properties</a>
+        <a href="contact.php">Contact</a>
+        <a class="active" href="login.php">Log In</a>
+        <a href="register.php">Register</a>
+    </div>
 </body>
 </html>>
