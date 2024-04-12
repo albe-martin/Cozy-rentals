4f89c5f4e39c00fc01c156ce7d46bb786778585b
PGN9 <pigeonQ0627@gmail.com>
2024-04-12 13:40:24 -0600

    fixed

--- postnewproperty.php
+++ postnewproperty.php
@@ -58,6 +58,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--style-->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Post Property</title>
 </head>
