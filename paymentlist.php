f1862f6345936230281f076831271f8645322f41
PGN9 <pigeonQ0627@gmail.com>
2024-04-12 20:38:15 -0600

    payment list basic

--- paymentlist.php
+++ paymentlist.php
@@ -44,7 +44,69 @@
   padding-left: 10px;
   padding-right: 10px;
 }
+
+/* main content */
+.main {
+  height:100%;
+  margin-top: 60px;
+  padding: 0px 10px;
+}
+
+/* Property Card */
+.card {
+  border: 3px solid transparent;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin-bottom: 20px;
+  text-align: left;
+  font-family: arial;
+  padding: 2px 16px;
+}
+
+.card:hover {
+  border: 3px solid tan;
+}
+
 </style>
+
+<?php
+// PHP FUNCTIONS
+function displayPayment($pid) {
+  // this pid = payment id
+  include "db_connect.php";
+  $sql = "SELECT * FROM payment WHERE Payment_id= $pid ";
+	$result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    $title = "Payment ID ".$pid;
+?>
+  <div class="card">
+    <?php echo "<h2>$title</h2>";
+    echo "<p>Amount: $".$row['amount']."</p>";
+    echo "<p>Card #: **** **** **** ".substr($row['card_num'], -4)."</p>";
+    echo "<p>Rented Property ID: ".$row['property_id']."</p>";
+
+    $sql_owner = "SELECT * FROM `property` WHERE Property_id= '$row[property_id]' ";
+    $result_owner= mysqli_query($conn, $sql_owner);
+    if (mysqli_num_rows($result_owner) > 0) {
+
+    } else if (mysqli_num_rows($result_owner) === 0) {
+
+    }
+    ?> 
+  </div>
+<?php
+  } else {
+    // error: payment not exist
+    $err_msg = "Payment ID ".$pid." does not exist.";
+?>
+    <div class="card">
+      <?php echo "<h2>Error</h2>"; ?>
+      <?php echo "<p>$err_msg</p>"; ?>
+    </div>
+<?php
+  }
+}
+?>
 
 <head>
     <meta charset="UTF-8">
@@ -72,6 +134,29 @@
         header("Location: index.php");  // redirect back to index
       }
     ?>
+
+      <!-- Page content -->
+      <div class="main">
+      <?php
+        // display payments
+        include "db_connect.php";
+        $sql = "SELECT * FROM pay WHERE Clients_email = '$_SESSION[email]' "; 
+	      $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+          // display every properties
+          while($row = $result->fetch_assoc()) {
+            displayPayment($row['payment_id']);
+          }
+        } else if (mysqli_num_rows($result) === 0){
+          // no payments to display
+          ?>
+          <div class="card">
+            <h2>"No payments in your record.."</h2>
+          </div>
+          <?php
+        }
+      ?>
+    </div>
 
 </body>
 </html>
