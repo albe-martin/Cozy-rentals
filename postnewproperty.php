ee73bd645edbe5970b41da9341bcd69db679cc7e
PGN9 <pigeonQ0627@gmail.com>
2024-04-13 15:33:58 -0600

    add more property type

--- postnewproperty.php
+++ postnewproperty.php
@@ -132,6 +132,9 @@ button:hover {
                     <option value="Apartment">Apartment</option>
                     <option value="House">House</option>
                     <option value="Condo">Condo</option>
+                    <option value="Mansion">Mansion</option>
+                    <option value="Basement">Basement</option>
+                    <option value="Duplex">Duplex</option>
                 </select>
             </div>
             <div class="form-group">
