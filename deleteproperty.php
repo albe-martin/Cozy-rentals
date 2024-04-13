c40eafbfe3f5d1cb5c9e0ef86bf8a8ca1456344b
albe <albe.martin@ucalgary.ca>
2024-04-12 20:11:11 -0600

    updated deleteproperty

--- deleteproperty.php
+++ deleteproperty.php
@@ -10,7 +10,7 @@ if (isset($_POST['delete'])) {
 
         try {
             // SQL to delete a record from own table
-            $sql_own = "DELETE FROM own WHERE Property_id = ?";
+            $sql_own = "DELETE FROM Own WHERE Property_id = ?";
             $stmt_own = mysqli_prepare($conn, $sql_own);
             mysqli_stmt_bind_param($stmt_own, 'i', $delete_id);
             mysqli_stmt_execute($stmt_own);
@@ -24,11 +24,26 @@ if (isset($_POST['delete'])) {
             mysqli_stmt_close($stmt_interior);
 
             // SQL to delete a record from property table
-            $sql_property = "DELETE FROM property WHERE Property_id = ?";
+            $sql_property = "DELETE FROM Property WHERE Property_id = ?";
             $stmt_property = mysqli_prepare($conn, $sql_property);
             mysqli_stmt_bind_param($stmt_property, 'i', $delete_id);
             mysqli_stmt_execute($stmt_property);
             mysqli_stmt_close($stmt_property);
+
+            //SQL to delete a record from Watchlist table
+            $sql_watchlist = "DELETE FROM Watchlist WHERE Property_id = ?";
+            $stmt_watchlist = mysqli_prepare($conn, $sql_watchlist);
+            mysqli_stmt_bind_param($stmt_watchlist, 'i', $delete_id);
+            mysqli_stmt_execute($stmt_watchlist);
+            mysqli_stmt_close($stmt_watchlist);
+
+            //SQL to delete a record from Showlist table
+            $sql_showlist = "DELETE FROM Showlist WHERE Property_id = ?";
+            $stmt_showlist = mysqli_prepare($conn, $sql_showlist);
+            mysqli_stmt_bind_param($stmt_showlist, 'i', $delete_id);
+            mysqli_stmt_execute($stmt_showlist);
+            mysqli_stmt_close($stmt_showlist);
+
 
             // Commit transaction
             mysqli_commit($conn);
